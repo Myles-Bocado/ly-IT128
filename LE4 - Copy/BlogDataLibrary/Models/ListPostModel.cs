31faba25b5984e1e8d1fb27af5b0e3b8d191f863
Myles-Bocado <bocadomiles@gmail>
2023-04-28 12:46:27 -0700

    Added LE6 files

---
+++ LE4 - Copy/BlogDataLibrary/Models/ListPostModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogDataLibrary.Models
+{
+    public class ListPostModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
